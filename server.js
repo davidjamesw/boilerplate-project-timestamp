22d6826d78e53a44d41ccfeb15e86d1f82369115
David Wright <davidjwright8@gmail.com>
2021-01-27 09:22:11 +0000

    Start application on 8080
    I've added the various resources needed to start the application on port 8080

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// load env variables
+const dotenv = require("dotenv")
+
+dotenv.config()
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
