bb3021a11a7f400c980330d0d165ad3b5b426e84
David Wright <davidjwright8@gmail.com>
2021-01-28 09:11:58 +0000

    Move everything into server.js
    As this application will be fairly simple, having everything in one place
    is probably ok.

--- server.js
+++ server.js
@@ -18,20 +18,20 @@ dotenv.config()
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
